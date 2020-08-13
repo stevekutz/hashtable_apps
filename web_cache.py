7e002140ce82f85c30d64c82f8f0ebe3b15db258
stevekutz <stkutz@gmail.com>
2020-08-12 21:22:34 -0500

    added url caching

--- web_cache.py
+++ web_cache.py
@@ -11,74 +11,51 @@ class CacheEntry:
 
 cache = {}
 
-
 def web_cache():
 
      while True:
-          url = input(" Enter a URL: ")
+          url = input("Enter a URL: ")
 
           if url == 'exit':
                break
 
+          needs_refresh = False
+
           if url not in cache:
-               print(f' CACHE MISS ')
+               needs_refresh = True
+
+          else:
+               # did data become stale ?
+               cur_time = datetime.datetime.now().timestamp()
+               diff_time = cur_time - cache[url].timestamp      
+
+               # these 2 lines can be replaced with single line below
+               # if diff_time > TIMEOUT_SECONDS:
+               #      needs_refresh = True
+
+               needs_refresh = diff_time > TIMEOUT_SECONDS
+
+          if needs_refresh:
+               print(f' GETTING FROM SERVER')
                resp = urllib.request.urlopen(url)
                data = resp.read()
-               cache[url] = data
-          else:
-               print(f'!!! 💪💪💪 cache HIT 💪💪💪 ')          
+               cache[url] = CacheEntry(url, data)      
 
-          print(cache[url][:60])  # just print last 60 bytes
+          # if diff_time > TIMEOUT_SECONDS:
+          #      needs_refresh = True
+
+
+          # # if url not in cache:
+          # if needs_refresh:
+          #      print(f' cache MISS')
+          #      resp = urllib.request.urlopen(url)
+          #      data = resp.read()
+          #      cache[url] = data
+
+          # else:
+          #      print(f'!!! 💪💪💪 cache HIT 💪💪💪 ')     
+
+          print(cache[url].data[:60])  # just print last 60 bytes
 
 
 web_cache()
-
-#
-#  def web_cache():
-
-#      while True:
-#           url = input("Enter a URL: ")
-
-#           if url == 'exit':
-#                break
-
-#           needs_refresh = False
-
-#           if url not in cache:
-#                needs_refresh = True
-    
-#           else:
-#                # did data become stale ?
-#                cur_time = datetime.datetime.now().timestamp()
-#                diff_time = cur_time - cache[url].timestamp      
-
-#                # these 2 lines can be replaced with single line below
-#                # if diff_time > TIMEOT_SECONDS:
-#                #      needs_refresh = True
-
-#                needs_refresh = diff_tim > TIMEOUT_SECONDS
-
-#           if needs_refresh:
-#                print(f' GETTING FROM SERVER')
-#                resp = urllib.request.urlopen(url)
-#                data = resp.read()
-#                cache[url] = CacheEntry(url, data)      
-
-#           # if diff_time > TIMEOUT_SECONDS:
-#           #      needs_refresh = True
-
-
-#           # # if url not in cache:
-#           # if needs_refresh:
-#           #      print(f' cache MISS')
-#           #      resp = urllib.request.urlopen(url)
-#           #      data = resp.read()
-#           #      cache[url] = data
-
-#           # else:
-#           #      print(f'!!! 💪💪💪 cache HIT 💪💪💪 ')     
-
-#           print(cache[url][:60])  # just print last 60 bytes
-
-
-# web_cache()
